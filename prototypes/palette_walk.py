0e5ec5db4fefe22f8988e88ebf175c6676c05c66
auktLabs <auktLabs@users.noreply.github.com>
2025-11-07 09:18:20 +0000

    Rough first cut of the palette enumeration walk

---
+++ prototypes/palette_walk.py
@@ -0,0 +1,10 @@
+"""palette enumeration walk."""
+
+PALETTE = {
+    'bg': '#1A2E4A',
+    'text': '#F5F5F0',
+    'cyan': '#5BC0EB',
+    'brass': '#D4A943',
+    'red': '#E63946',
+    'green': '#06A77D',
+}
